425454c3d48a8c9a3bf6b0f042de2246f7945ec0
dermatobia <webblewebble@gmail.com>
2016-07-05 09:27:52 -0400

    convert to es6. create classes

--- snake.js
+++ snake.js
@@ -1,29 +1,68 @@
+const squareSize = 10;
+
 class Game {
   constructor() {
+    let canvas = document.getElementById('screen');
+    this.ctx = canvas.getContext('2d');
+
+    this.start();
   }
 
   start() {
+    let snake = new Snake(this);
+    let food = new Food(this);
+    snake.draw();
+    food.draw();
+
+    this.keyboarder();
+  }
+
+  draw(posX, posY, color) {
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(posX, posY, squareSize, squareSize);
+  }
+
+  keyboarder() {
+    document.addEventListener('keydown', (event) => {
+      switch (event.key) {
+        case 'ArrowUp':
+          console.log('Atas');
+          break;
+        case 'ArrowRight':
+          console.log('Kanan');
+          break;
+        case 'ArrowDown':
+          console.log('Bawah');
+          break;
+        case 'ArrowLeft':
+          console.log('Kiri');
+          break;
+      }
+    })
+  }
+}
+
+class Food {
+  constructor(game) {
+    this.game = game;
+  }
+
+  draw() {
+    this.game.draw(300, 300, 'red');
   }
 }
 
 class Snake {
-  constructor() {
-    this.width = 10;
-    this.height = 10;
+  constructor(game) {
+    this.game = game;
   }
 
-  drawBody() {
-    var canvas = document.getElementById('screen');
-    var ctx = canvas.getContext('2d');
-
-    ctx.fillStyle = 'red';
-    ctx.fillRect(200, 200, this.width, this.height);
-    ctx.fillStyle = 'green';
-    ctx.fillRect(300, 300, this.width, this.height);
+  draw() {
+    this.game.draw(200, 200, 'green');
   }
 }
 
-window.addEventListener('load', function() {
-  let snake = new Snake();
-  snake.drawBody();
+
+window.addEventListener('load', () => {
+  let game = new Game();
 })
